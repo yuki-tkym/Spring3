bdaeb855570f2c4c49718f7b7e1b4cb98f885895
p000h16181 <p000h16181@A045243447.rfg.rfgad.ricoh.com>
2018-12-04 10:14:20 +0900

    first commit

---
+++ example_spmvc/src/main/java/com/example/demo/Car.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public interface Car {
+	/*
+	 * ‘–‚é
+	 */
+	public void run();
+	/*
+	 * ‚Æ‚Ü‚é
+	 */
+	public void stop();
+}
---
+++ example_spmvc/src/main/java/com/example/demo/NormalCar.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class NormalCar implements Car{
+	private String velocity;
+	private String distance;
+	
+	public void setVelocity(String velocity) {
+		this.velocity = velocity;
+	}
+	
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+	
+	public void run() {
+		System.out.println("走行速度は"+ velocity + "です。");
+	}
+	
+	public void stop() {
+		System.out.println("走行距離は"+ distance + "です。");
+	}
+}
